ca462cde3b5a6f0a1514a6cd425ce5b31aec2d17
svyat1234 <75384046+svyat1234@users.noreply.github.com>
2021-05-20 21:34:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,46 @@ function videoPlay(event) {
   event.target.playVideo();
 }
 
+function plus(){
+	
+	var num1, num2, num3, num4, result;
+	
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+	
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	num3 = document.getElementById('n3').value;
+	num3 = parseInt(num3);
+
+	num4 = document.getElementById('n4').value;
+	num4 = parseInt(num4);
+	
+	result = num1 * 123 + num2 * 321 + num3 * 1000 + num4 * 1500 + ' Рублей';
+	
+	document.getElementById('out').innerHTML = result;
+}
+
+ // Настройка модального окна
+ var modal = $('.modal__calc'), 
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__calc__close'),
+        switchModal = () => {
+            modal.toggleClass('modal__calc--visible');
+        } ;
+    modalBtn.on('click', switchModal );
+    closeBtn.on('click', switchModal);
+    
+    // пропадает (и почему то появляется) при нажатии на Esc
+    $(document).keyup(function (e) {
+        if (e.key === "Escape") {
+            modal.removeClass('modal__calc--visible');
+        }
+    });
+    // Скрывает модально окно при нажатии вне него
+    $(document).click(function (event) {
+        if ($(event.target).is('.modal__calc')) {
+            modal.toggleClass('modal__calc--visible');
+        }
+    });
